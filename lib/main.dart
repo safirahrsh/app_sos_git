a726df33c4ded53da3693ec9271a59cf31dede04
Safirah Rashifah <safirahrashifah@gmail.com>
2021-04-25 13:03:50 +0700

    ganti sos application ke sos aja

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SOS Application',
+      title: 'SOS',
       home: AppNavBar(),
     );
   }
