db974a11423fc4c6be3096af4721ef6a125fbb1a
Safirah Rashifah <safirahrashifah@gmail.com>
2021-04-25 12:55:34 +0700

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SOS',
+      title: 'SOS Application',
       home: AppNavBar(),
     );
   }
