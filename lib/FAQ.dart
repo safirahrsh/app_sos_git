4d60d1da779ac8791f7aef632d6c7fcb757403ce
aditya <appadityapratamaputra@gmail.com>
2021-04-26 11:36:28 +0700

    FAQ help menus fix

--- lib/FAQ.dart
+++ lib/FAQ.dart
@@ -10,24 +10,23 @@ class _FaqPageState extends State<FaqPage> {
   Widget build(BuildContext context) {
     MediaQueryData queryData = MediaQuery.of(context);
     return new Scaffold(
-      body: SafeArea(
-        minimum: EdgeInsets.only(
-            right: queryData.size.width * 5 / 100,
-            left: queryData.size.width * 5 / 100),
-        child: Center(
-            child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                  top: queryData.size.height * 7 / 100,
-                  bottom: queryData.size.height * 7 / 100),
-              child: Text(
-                'Help and FAQ',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
+      body: Center(
+          child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+                top: queryData.size.height * 7 / 100,
+                bottom: queryData.size.height * 7 / 100),
+            child: Text(
+              'Help and FAQ',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            Align(
-              alignment: Alignment.centerLeft,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(queryData.size.width * 5 / 100, 0,
+                  queryData.size.width * 5 / 100, 0),
               child: Text(
                 'HELP',
                 style: TextStyle(
@@ -36,106 +35,109 @@ class _FaqPageState extends State<FaqPage> {
                     fontSize: 15),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: queryData.size.height * 2 / 100,
-                  bottom: queryData.size.height * 2 / 100),
-              child: Container(
-                width: queryData.size.width,
-                height: queryData.size.height * 20 / 100,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: queryData.size.height * 1 / 100,
-                        bottom: queryData.size.height * 1 / 100,
-                        right: queryData.size.width * 2 / 100,
-                        left: queryData.size.width * 2 / 100,
-                      ),
-                      child: SizedBox(
-                        width: queryData.size.width * 40 / 100,
-                        child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Help 1',
-                              style: TextStyle(color: Colors.red),
-                            ),
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(18.0))),
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white),
-                              shadowColor:
-                                  MaterialStateProperty.all<Color>(Colors.grey),
-                            )),
-                      ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                top: queryData.size.height * 2 / 100,
+                bottom: queryData.size.height * 2 / 100),
+            child: Container(
+              width: queryData.size.width,
+              height: queryData.size.height * 20 / 100,
+              child: ListView(
+                padding: EdgeInsets.fromLTRB(queryData.size.width * 5 / 100, 0,
+                    queryData.size.width * 5 / 100, 0),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: queryData.size.height * 1 / 100,
+                      bottom: queryData.size.height * 1 / 100,
+                      right: queryData.size.width * 2 / 100,
+                      left: queryData.size.width * 2 / 100,
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: queryData.size.height * 1 / 100,
-                        bottom: queryData.size.height * 1 / 100,
-                        right: queryData.size.width * 2 / 100,
-                        left: queryData.size.width * 2 / 100,
-                      ),
-                      child: SizedBox(
-                        width: queryData.size.width * 40 / 100,
-                        child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Help 2',
-                              style: TextStyle(color: Colors.red),
-                            ),
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(18.0))),
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white),
-                              shadowColor:
-                                  MaterialStateProperty.all<Color>(Colors.grey),
-                            )),
-                      ),
+                    child: SizedBox(
+                      width: queryData.size.width * 40 / 100,
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Help 1',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0))),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            shadowColor:
+                                MaterialStateProperty.all<Color>(Colors.grey),
+                          )),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: queryData.size.height * 1 / 100,
-                        bottom: queryData.size.height * 1 / 100,
-                        right: queryData.size.width * 2 / 100,
-                        left: queryData.size.width * 2 / 100,
-                      ),
-                      child: SizedBox(
-                        width: queryData.size.width * 40 / 100,
-                        child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              'Help 3',
-                              style: TextStyle(color: Colors.red),
-                            ),
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(18.0))),
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.white),
-                              shadowColor:
-                                  MaterialStateProperty.all<Color>(Colors.grey),
-                            )),
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: queryData.size.height * 1 / 100,
+                      bottom: queryData.size.height * 1 / 100,
+                      right: queryData.size.width * 2 / 100,
+                      left: queryData.size.width * 2 / 100,
                     ),
-                  ],
-                ),
+                    child: SizedBox(
+                      width: queryData.size.width * 40 / 100,
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Help 2',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0))),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            shadowColor:
+                                MaterialStateProperty.all<Color>(Colors.grey),
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: queryData.size.height * 1 / 100,
+                      bottom: queryData.size.height * 1 / 100,
+                      right: queryData.size.width * 2 / 100,
+                      left: queryData.size.width * 2 / 100,
+                    ),
+                    child: SizedBox(
+                      width: queryData.size.width * 40 / 100,
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Help 3',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0))),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            shadowColor:
+                                MaterialStateProperty.all<Color>(Colors.grey),
+                          )),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Align(
-              alignment: Alignment.centerLeft,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(queryData.size.width * 5 / 100, 0,
+                  queryData.size.width * 5 / 100, 0),
               child: Text(
                 'FREQUENTLY ASKED QUESTION',
                 style: TextStyle(
@@ -144,76 +146,78 @@ class _FaqPageState extends State<FaqPage> {
                     fontSize: 15),
               ),
             ),
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  Card(
-                    child: ExpansionTile(
-                      title: Text(
-                        'Question 1',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      children: [
-                        Container(
-                          height: queryData.size.height * 8 / 100,
-                          child: Align(
-                            child: Text(
-                              'Answer 1',
-                              style: TextStyle(fontSize: 15),
-                            ),
-                            alignment: Alignment.center,
-                          ),
-                        )
-                      ],
+          ),
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.fromLTRB(queryData.size.width * 5 / 100, 0,
+                  queryData.size.width * 5 / 100, 0),
+              shrinkWrap: true,
+              children: [
+                Card(
+                  child: ExpansionTile(
+                    title: Text(
+                      'Question 1',
+                      style: TextStyle(fontSize: 15),
                     ),
+                    children: [
+                      Container(
+                        height: queryData.size.height * 8 / 100,
+                        child: Align(
+                          child: Text(
+                            'Answer 1',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          alignment: Alignment.center,
+                        ),
+                      )
+                    ],
                   ),
-                  Card(
-                    child: ExpansionTile(
-                      title: Text(
-                        'Question 2',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      children: [
-                        Container(
-                          height: queryData.size.height * 8 / 100,
-                          child: Align(
-                            child: Text(
-                              'Answer 2',
-                              style: TextStyle(fontSize: 15),
-                            ),
-                            alignment: Alignment.center,
-                          ),
-                        )
-                      ],
+                ),
+                Card(
+                  child: ExpansionTile(
+                    title: Text(
+                      'Question 2',
+                      style: TextStyle(fontSize: 15),
                     ),
+                    children: [
+                      Container(
+                        height: queryData.size.height * 8 / 100,
+                        child: Align(
+                          child: Text(
+                            'Answer 2',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          alignment: Alignment.center,
+                        ),
+                      )
+                    ],
                   ),
-                  Card(
-                    child: ExpansionTile(
-                      title: Text(
-                        'Question 3',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      children: [
-                        Container(
-                          height: queryData.size.height * 8 / 100,
-                          child: Align(
-                            child: Text(
-                              'Answer 3',
-                              style: TextStyle(fontSize: 15),
-                            ),
-                            alignment: Alignment.center,
-                          ),
-                        )
-                      ],
+                ),
+                Card(
+                  child: ExpansionTile(
+                    title: Text(
+                      'Question 3',
+                      style: TextStyle(fontSize: 15),
                     ),
-                  )
-                ],
-              ),
-            )
-          ],
-        )),
-      ),
+                    children: [
+                      Container(
+                        height: queryData.size.height * 8 / 100,
+                        child: Align(
+                          child: Text(
+                            'Answer 3',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          alignment: Alignment.center,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
